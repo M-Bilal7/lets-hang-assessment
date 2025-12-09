831da5d3ea48277e788ecf6727fe913eb397bfc7
M-Bilal7 <work.mohd.bilal@gmail.com>
2025-12-09 14:44:25 -0500

    Refactor CostInput to use currency utility

--- components/CostInput.tsx
+++ components/CostInput.tsx
@@ -1,4 +1,5 @@
 import { forwardRef } from 'react';
+import { formatCurrency } from '@/lib/utils/currency';
 
 interface CostInputProps {
   value: string;
@@ -8,20 +9,6 @@ interface CostInputProps {
 
 const CostInput = forwardRef<HTMLInputElement, CostInputProps>(
   ({ value, onChange, error }, ref) => {
-    const formatCurrency = (input: string): string => {
-      const digitsOnly = input.replace(/[^\d.]/g, '');
-      const parts = digitsOnly.split('.');
-
-      if (parts.length > 2) {
-        return parts[0] + '.' + parts.slice(1).join('');
-      }
-
-      if (parts[1] && parts[1].length > 2) {
-        parts[1] = parts[1].substring(0, 2);
-      }
-
-      return parts.join('.');
-    };
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       const formatted = formatCurrency(e.target.value);
